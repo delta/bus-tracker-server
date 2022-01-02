28f2019dffc8d0155968350fd6df94632f30a2b1
Ashwin125 <ashwinvishwakarma125@gmail.com>
2022-01-02 17:17:20 +0530

    fix(models): mongodb auto-index-creation added
        * auto-index-creation added to Location.java, User.java,
          Vehicle.java with @document annotation
    
        * application.properties.example updated accordingly

--- src/main/java/edu/nitt/delta/bustracker/model/Location.java
+++ src/main/java/edu/nitt/delta/bustracker/model/Location.java
@@ -5,12 +5,14 @@ import lombok.Data;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 @Builder
+@Document
 public class Location {
     @Id private String id;
 
--- src/main/java/edu/nitt/delta/bustracker/model/Vehicle.java
+++ src/main/java/edu/nitt/delta/bustracker/model/Vehicle.java
@@ -5,9 +5,11 @@ import lombok.Data;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
+@Document
 public class Vehicle {
     @Id private String id;
 
